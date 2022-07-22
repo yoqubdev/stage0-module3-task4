4b31ba700d9bb220f80d18f63cedc86a12d7c717
Yoqub <yyboboraximov@gmail.com>
2022-07-22 10:02:54 +0500

    task2_primitives

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        long a = 9 ;
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99 , h = 104 , a = 97 , r = 114;
+        System.out.println(c+" "+h+" "+a+" "+r);
     }
 }
